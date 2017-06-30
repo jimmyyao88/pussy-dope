b3a801e7ca0ba4d4d99582a1a5f8ea5717d145d7
Jimmy <rollingdog@163.com>
2017-06-30 12:22:40 +0800

    edit comment:我不会这样轻易的猫带

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,7 +1,5 @@
-//=========================================================
-//  CONSTANTS
-//---------------------------------------------------------
-export const APP_NAME = 'soundcloud-redux';
+
+export const APP_NAME = '我不会这样轻易的猫带';
 
 
 //=====================================
@@ -18,16 +16,12 @@ export const PAGINATION_LIMIT = 60;
 export const PAGINATION_PARAMS = `limit=${PAGINATION_LIMIT}&linked_partitioning=1`;
 
 
-//=====================================
-//  IMAGES
-//-------------------------------------
+
 export const IMAGE_DEFAULT_SIZE = 'large.jpg';
 export const IMAGE_XLARGE_SIZE = 't500x500.jpg';
 
 
-//=====================================
-//  PLAYER
-//-------------------------------------
+
 export const PLAYER_INITIAL_VOLUME = 10;
 export const PLAYER_MAX_VOLUME = 100;
 export const PLAYER_VOLUME_INCREMENT = 5;
@@ -35,9 +29,7 @@ export const PLAYER_VOLUME_INCREMENT = 5;
 export const PLAYER_STORAGE_KEY = `${APP_NAME}:player`;
 
 
-//=====================================
-//  TRACKLISTS
-//-------------------------------------
+
 export const FEATURED_TRACKLIST_ID = 'featured';
 export const FEATURED_TRACKLIST_USER_ID = 185676792;
 
@@ -46,8 +38,6 @@ export const SESSION_TRACKLIST_ID = 'session';
 export const TRACKS_PER_PAGE = 12;
 
 
-//=====================================
-//  WAVEFORMS
-//-------------------------------------
+
 export const WAVEFORM_IMAGE_HOST = 'w1.sndcdn.com';
 export const WAVEFORM_JSON_HOST = 'wis.sndcdn.com';
